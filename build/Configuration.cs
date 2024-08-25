3af793d59db082d2338c3f618160f757f7d2eac4
Michaël Hompus <michael@hompus.nl>
2024-08-25 18:33:13 +0200

    Add NUKE build

---
+++ build/Configuration.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel;
+using Nuke.Common.Tooling;
+
+[TypeConverter(typeof(TypeConverter<Configuration>))]
+public class Configuration : Enumeration
+{
+    public static readonly Configuration Debug = new() { Value = nameof(Debug) };
+    public static readonly Configuration Release = new() { Value = nameof(Release) };
+    public static readonly Configuration CI = new() { Value = nameof(CI) };
+
+    public static implicit operator string(Configuration configuration)
+    {
+        return configuration.Value;
+    }
+}
