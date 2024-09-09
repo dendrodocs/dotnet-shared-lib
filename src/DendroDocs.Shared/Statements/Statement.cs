8b41318c617fe0fccd2ec1929ab91f73eef21c5d
Michaël Hompus <michael@hompus.nl>
2024-09-09 21:02:58 +0200

    Define derived types for statements

--- src/DendroDocs.Shared/Statements/Statement.cs
+++ src/DendroDocs.Shared/Statements/Statement.cs
@@ -1,5 +1,13 @@
 namespace DendroDocs;
 
+[JsonDerivedType(typeof(ForEach), typeDiscriminator: "DendroDocs.ForEach, DendroDocs.Shared")]
+[JsonDerivedType(typeof(If), typeDiscriminator: "DendroDocs.If, DendroDocs.Shared")]
+[JsonDerivedType(typeof(IfElseSection), typeDiscriminator: "DendroDocs.IfElseSection, DendroDocs.Shared")]
+[JsonDerivedType(typeof(Switch), typeDiscriminator: "DendroDocs.Switch, DendroDocs.Shared")]
+[JsonDerivedType(typeof(SwitchSection), typeDiscriminator: "DendroDocs.SwitchSection, DendroDocs.Shared")]
+[JsonDerivedType(typeof(InvocationDescription), typeDiscriminator: "DendroDocs.InvocationDescription, DendroDocs.Shared")]
+[JsonDerivedType(typeof(ReturnDescription), typeDiscriminator: "DendroDocs.ReturnDescription, DendroDocs.Shared")]
+[JsonDerivedType(typeof(AssignmentDescription), typeDiscriminator: "DendroDocs.AssignmentDescription, DendroDocs.Shared")]
 public abstract class Statement
 {
     [Newtonsoft.Json.JsonProperty(ItemTypeNameHandling = Newtonsoft.Json.TypeNameHandling.Objects)]
